8edc2e61dcf183a622471e025da40bace1a22680
gilcierweb <gilcier06@yahoo.com.br>
2025-04-17 02:12:37 -0300

    add controller users

---
+++ application/controllers/api/users/Users_controller.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Users_controller extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('user_model');		
+	}
+}
